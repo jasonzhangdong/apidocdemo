98cbe37d5569c5507b783c73af27b6a2e5b7973e
jasonzhangdong <519705069@qq.com>
2017-06-23 14:35:52 +0800

    apidoc

---
+++ api/example.js
@@ -0,0 +1,242 @@
+/**
+ * @apiGroup User
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * 
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+/**
+ * @apiGroup User2
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+
+/**
+ * @apiGroup User3
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * 
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+
+/**
+ * @apiGroup User4
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * 
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+
+/**
+ * @apiGroup User5
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * 
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+
+/**
+ * @apiGroup User6
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * 
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+
+/**
+ * @apiGroup User7
+ * @apiDescription 用于用户登录
+ * @api {GET} /user?use&age=&name=&sex=
+ * @apiVersion 1.6.3
+ * @apiHeader {String} access-key Users—access-key
+ * @apiParam {String} user 用户名
+ * @apiParam {String} age 年龄
+ * @apiParam {String} name 姓名
+ * @apiParam {String} sex 性别
+ * 
+ * @apiParam {String} [firstname]  Optional Firstname of the User.
+ * @apiParam {String} lastname     Mandatory Lastname.
+ * @apiParam {String} country="DE" Mandatory with default value "DE".
+ * @apiParam {Number} [age=18]     Optional Age with default 18.
+ *
+ * @apiExample {url} 例如
+ * http://localhost/user?user=zds&age=22&name=jaosn&sex=男
+ * 
+ * @apiSuccessExample {json} 成功返回
+ *      {
+ *        "id":123444,
+ *        "name":jason,
+ *        "age":33
+ *      }
+ * 
+ * @apiErrorExample {json} 错误返回:
+ *     {
+ *       "error": "notfound"
+ *     }
+ */
+
+
+
+
+
+
+
+
+
